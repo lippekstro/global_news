82607279c5dfdacd11044f0d271d81a42a373e0d
lippekstro <fellipecastro13@hotmail.com>
2023-02-01 20:38:31 -0300

    sobre

--- views/sobre.php
+++ views/sobre.php
@@ -2,8 +2,39 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . '/global_news/templates/cabecalho.php';
 ?>
 
-<div>
-
+<div class="container-sobre">
+    <section>
+        <h2>Sobre Nós</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Assumenda perspiciatis, fugit obcaecati nemo magni eligendi nesciunt quis laudantium. 
+            Et maxime fugit delectus inventore nostrum laborum nemo sequi enim, recusandae reiciendis.
+        </p>
+    </section>
+    <section>
+        <h2>Missão</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Assumenda perspiciatis, fugit obcaecati nemo magni eligendi nesciunt quis laudantium. 
+            Et maxime fugit delectus inventore nostrum laborum nemo sequi enim, recusandae reiciendis.
+        </p>
+    </section>
+    <section>
+        <h2>Visão</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Assumenda perspiciatis, fugit obcaecati nemo magni eligendi nesciunt quis laudantium. 
+            Et maxime fugit delectus inventore nostrum laborum nemo sequi enim, recusandae reiciendis.
+        </p>
+    </section>
+    <section>
+        <h2>Valores</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Assumenda perspiciatis, fugit obcaecati nemo magni eligendi nesciunt quis laudantium. 
+            Et maxime fugit delectus inventore nostrum laborum nemo sequi enim, recusandae reiciendis.
+        </p>
+    </section>
 </div>
 
 <?php
